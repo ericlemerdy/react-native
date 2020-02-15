389e6a7a342a36916e865386ed2c94f55d0824c4
Éric Le Merdy <eric@desktop-0cacv90.home>
2020-02-15 23:00:56 +0100

    Just a refac

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Main from './components/MainComponents';
+import Main from './components/MainComponent';
 
 export default function App() {
   return (
