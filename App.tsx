fb29c9126ea14b5b68e000d5d3588d958fa7799e
Éric Le Merdy <eric@desktop-0cacv90.home>
2020-02-15 22:55:05 +0100

    Remove Linear gradient

--- App.tsx
+++ App.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
 import Main from './components/MainComponents';
 
 export default function App() {
   return (
-    <LinearGradient style={styles.container} colors={['#8B5BF1', '#D1489E']} start={[0, 100]} end={[100, 0]}>
-      <Main />
-    </LinearGradient>
+    <Main />
   );
 }
 
